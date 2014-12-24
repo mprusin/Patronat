9e27c97eed76e8e95cc38e98c4480a01c7fd3cc1
mprusin <mprusin@wp.pl>
2014-12-24 02:10:19 +0100

    Nowe zadanie

---
+++ README.md
@@ -0,0 +1,7 @@
+Parser napisany w PHP, uruchamiany z poziomu HTML:
+<form action="parser.php"method="post">
+
+Wczytuje dostarczony plik: "CsvSampleOpenData Zadanie 1.csv", zlokalizowany w g³ównym katalogu.
+W wyniku powstaje plik: "Zadanie 1.json ".
+
+Czekam na uwagi ;)
---
+++ parser.php
@@ -0,0 +1,71 @@
+<DOCTYPE HTML>
+<html	lang="pl">
+<head>
+	<meta charset="utf-8"/>
+	<title>Pokaż dane</title>
+</head>
+
+<body>
+	<?php
+		ini_set("auto_detect_line_endings", true);
+		
+		$file_in = "CsvSampleOpenData Zadanie 1.csv";
+
+		$keys = array();
+		$newArray = array();
+
+		function csvToArray($file, $delimiter) { 
+		  if (($handle = fopen($file, 'r')) !== FALSE) { 
+			$i = 0; 
+			while (($lineArray = fgetcsv($handle, 0, $delimiter, '"')) !== FALSE) { 
+				for ($j = 0; $j < count($lineArray); $j++) { 
+				$arr[$i][$j] = $lineArray[$j]; 
+				} 
+			  $i++; 
+			} 
+			fclose($handle); 
+		  } 
+		  return $arr; 
+		} 
+
+		$data = csvToArray($file_in, ',');
+		$count = count($data) - 1;
+		$labels = array_shift($data);  
+		foreach ($labels as $label) {
+			$keys[] = $label;
+		}
+		
+		$count_keys = count($keys) ;
+		//print_r("$count ,$count_keys");
+		
+		$keys[] = 'id';
+
+		for ($i = 0; $i < $count; $i++) {
+		  $data[$i][] = $i;
+		}
+
+		for ($j = 0; $j < $count; $j++) {
+			//if ($count = $count_keys) {
+			
+			$d = array_combine($keys, $data[$j]);
+			
+			//} else {
+			//	echo "Błąd";
+			//}
+		//print_r($d);
+		$newArray[$j] = $d;
+		}
+		$dane= json_encode($newArray);
+
+		//Zapis danych do pliku
+	
+		$file_out = fopen("Zadanie 1.json","c");
+		fputs($file_out, $dane);
+		fclose($file_out);
+		print_r("Utworzono plik: Zadanie 1.json");
+		
+	?>
+
+
+</body>
+</html>
