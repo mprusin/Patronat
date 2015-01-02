1353ff5566eec4e9e2d01827df91ab731843c98a
mprusin <mprusin@wp.pl>
2015-01-02 22:26:46 +0100

    Revert "Revert "MongoDB""
    This reverts commit eb450074c9491843d7a0dba45271df0156ac7242.

--- parser.php
+++ parser.php
@@ -13,6 +13,14 @@
 
 		$keys = array();
 		$newArray = array();
+		
+		// Otwieranie bazy
+		$connection = new Mongo();
+		$db = $connection->selectDB( "MyBase" );
+		$collection = $db->selectCollection( "Transaction" );
+		
+		//echo $collection->count();
+		$collection->remove();
 
 		function csvToArray($file, $delimiter) { 
 		  if (($handle = fopen($file, 'r')) !== FALSE) { 
@@ -25,27 +33,37 @@
 			} 
 			fclose($handle); 
 		  } 
-		  return $arr; 
+			return $arr; 
 		} 
 
 		$data = csvToArray($file_in, ',');
-		$count = count($data) - 1;
+		/* debagger
+		echo "<pre>";
+			print_r($data);
+		echo "</pre>";
+		*/
+		$n = count($data);
+		
+		$count = count($data) -1;
 		$labels = array_shift($data);  
 		foreach ($labels as $label) {
 			$keys[] = $label;
 		}
 		
-		$count_keys = count($keys) ;
+		$count_keys = count($keys);
 		//print_r("$count ,$count_keys");
 		
 		$keys[] = 'id';
 
 		for ($i = 0; $i < $count; $i++) {
-		  $data[$i][] = $i;
+			$data[$i][] = $i;
 		}
 
 		for ($j = 0; $j < $count; $j++) {
 			//if ($count = $count_keys) {
+			
+			
+			
 			
 			$d = array_combine($keys, $data[$j]);
 			
@@ -54,16 +72,28 @@
 			//}
 		//print_r($d);
 		$newArray[$j] = $d;
+		$collection->insert($d);
 		}
 		$dane= json_encode($newArray);
-
+		
+		
+		
 		//Zapis danych do pliku
-	
+	/*
 		$file_out = fopen("Zadanie 1.json","c");
 		fputs($file_out, $dane);
 		fclose($file_out);
 		print_r("Utworzono plik: Zadanie 1.json");
-		
+		*/
+		// Wyświetlenie wszystkich wyników
+		$cursor = $collection->find();
+		foreach ($cursor as $id => $value) {
+			echo "$id: ";
+			var_dump( $value );
+		}
+		// pierwszego		
+		//$rangeQuery = array('id' => 2);
+		//print_r($collection->find().limit(1);
 	?>
 
 
